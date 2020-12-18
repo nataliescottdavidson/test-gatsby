5e48e36946b7e1d24d7fa9ef3b1b093c735c1a6a
Nat <davidson@cloudflare.com>
2020-12-18 16:10:43 -0500

    changes

--- project_here/src/pages/index.js
+++ project_here/src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h2>
-      🤓👽🤪🤓
+      🤓👽🤪
       This site shows multiple ways of using ENV variables as shown in the{' '}
       <a href="https://www.gatsbyjs.org/docs/environment-variables">
         documentation
